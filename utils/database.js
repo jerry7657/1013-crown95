ae6e01c3a3d1bd1befbe0dfc827467fa43eedea1
jerry7657 <jerry7657@gmail.com>
2021-10-13 20:37:42 +0800

    first commit

---
+++ utils/database.js
@@ -0,0 +1,24 @@
+const { Pool } = require('pg')
+
+const isProduction = process.env.NODE_ENV === "production"
+
+
+
+ const pool = new Pool({
+    // user: 'postgres',
+    // host: 'localhost',
+    // database: 'crown_95',
+    // password: '0000',
+    // port: '5432',
+    connectionString: isProduction ? process.env.DATABASE_URL : `postgresql://postgres:0000@localhost:5432/crown95`
+  });
+
+
+// pool.query('SELECT * from category', (err, res) => {
+//     console.log(JSON.stringify(res.rows));
+//     pool.end();
+// });
+
+module.exports = pool;
+
+// postgres://khfzlhxoepbske:99555b7187c1d7a1c8786afc1a258c2ff316c98751601af6021535ae0aaa1615@ec2-54-204-148-110.compute-1.amazonaws.com:5432/d9qfhqgum1484d
